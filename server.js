b3e59725c4b8eb5cd097afcd50b8b58582705a53
shivangbhardwaj2001 <shivangbhardwaj2001@gmail.com>
2022-05-08 01:40:45 +0530

    changed the port

--- server.js
+++ server.js
@@ -103,14 +103,14 @@ app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res,db)})
 
 
 
-app.listen(3000,()=>{
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT||3000,()=>{
+	console.log('app is running on port ${process.env.PORT}');
 })
 
 
 
 
-/*
+/* 
 
 / -->respose = this is working
 /signin --> POST = success/fail
